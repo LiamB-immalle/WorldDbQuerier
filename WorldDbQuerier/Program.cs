9aeb61a0aabf6cacf1f7789b2fd412e95ca36225
LiamB-immalle <liam.brangers@immalle.be>
2017-09-28 10:00:49 +0200

    WorldDbQuerier road to 0.3

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -7,6 +7,41 @@ namespace WorldDbQuerier
     {
         static string version = "0.1";
 
+        static void ConnectSql()
+        {
+
+        }
+
+        static void CountryAmount()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+            "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT count(*) FROM world.Country";
+
+            conn.Open();
+
+            Console.WriteLine("Amount o' country's : {0}", cmd.ExecuteScalar());
+        }
+
+        static void PrintCountry()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+            "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT Name FROM world.Country";
+            
+            conn.Open();
+
+            Console.WriteLine("");
+        }
+
         static void Main(string[] args)
         {
             if (args.Length > 0)
@@ -22,17 +57,8 @@ namespace WorldDbQuerier
                 }
             }
 
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString =
-            "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
-
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "SELECT count(*) FROM world.Country";
-
-            conn.Open();
-
-            Console.WriteLine("Amount o' country's : {0}", cmd.ExecuteScalar());
+            PrintCountry();
+            
         }
     }
 }
